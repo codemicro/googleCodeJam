8e114e5025e4d9b61dfaafc98d92cc2a7f1e5616
Tom <tom@tdpain.net>
2021-02-17 13:29:48 +0000

    [GCJ20 Quals] Nesting Depth implementation two

--- codeJam/2020/0-quals/nestingDepth.py
+++ codeJam/2020/0-quals/nestingDepth.py
@@ -2,9 +2,23 @@ test_cases = int(input().strip())
 
 for case_number in range(test_cases):
 
-    output_string = "".join([int(i) * "(" + i + int(i) * ")" for i in input().strip()])
+    current_parens_level = 0
 
-    while output_string.find(")(") != -1:
-        output_string = output_string.replace(")(", "")
+    input_string = "0" + input().strip() + "0"
+    output_string = ""
+
+    for char_index in range(1, len(input_string)):
+        n = int(input_string[char_index])
+
+        diff = n - current_parens_level
+        current_parens_level = n
+
+        bracket = "("
+        if diff < 0:
+            bracket = ")"
+
+        output_string += (bracket * abs(diff)) + str(n)
+
+    output_string = output_string[:-1]  # remove final extra zero
 
     print("Case #{}: {}".format(case_number+1, output_string))
