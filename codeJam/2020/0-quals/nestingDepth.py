bc82ce486052736d21521f468879338448f1293a
Tom <tom@tdpain.net>
2021-02-17 13:10:23 +0000

    [GCJ20 Quals] Optimise Nesting Depth

--- codeJam/2020/0-quals/nestingDepth.py
+++ codeJam/2020/0-quals/nestingDepth.py
@@ -1,31 +1,10 @@
 test_cases = int(input().strip())
 
-cases = []
+for case_number in range(test_cases):
 
-for test_number in range(test_cases):
-    source_string = input().strip()
-    cases.append(source_string)
-
-for case_number, data in enumerate(cases):
-    case_list = list(data)
-    
-    sequence = []
-    # loop below relies on the existence of at least one item in the list
-    sequence.append([case_list[0], 1])  
-    
-    for index, item in enumerate(case_list[1:]):
-        if sequence[-1][0] == item:
-            sequence[-1][1] += 1
-        else:
-            sequence.append([item, 1])
-
-    output_string = ""
-    for pair in sequence:
-        output_string += "(" * int(pair[0])
-        output_string += pair[0] * pair[1]
-        output_string += ")" * int(pair[0])
+    output_string = "".join([int(i) * "(" + i + int(i) * ")" for i in input().strip()])
 
     while output_string.find(")(") != -1:
         output_string = output_string.replace(")(", "")
-    
+
     print("Case #{}: {}".format(case_number+1, output_string))
